d571125f94102f5c2cdb2ce238f8c8583b20eb30
manoj2990 <manojmeena2990@gmail.com>
2025-03-16 14:10:56 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,40 @@
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const dbconnection = require('./configration/databaseConfigration')
-const {cloudinaryConnect} = require("../Backend/configration/cloudinaryConfigration");
+// const dbconnection = require('./configration/databaseConfigration')
+// const {cloudinaryConnect} = require("../Backend/configration/cloudinaryConfigration");
+// const app = require("./app");
+// const {PORT} = require('./constant');
+
+
+// cloudinaryConnect();
+    
+// dbconnection() 
+// .then( ()=>(
+//     app.listen( PORT || 5000 ,(req, res)=>(
+//         console.log(`server is running at port :${PORT}`)
+//     ))
+    
+// ))
+// .catch( (error)=>(
+//     console.log('App failed to listen due to DB connection failure:',error)
+// ))
+
+
+const { cloudinaryConnect } = require("../Backend/configration/cloudinaryConfigration");
 const app = require("./app");
-const {PORT} = require('./constant');
+const { PORT } = require('./constant');
 
-
+// Connect to Cloudinary and Database
 cloudinaryConnect();
-    
-dbconnection() 
-.then( ()=>(
-    app.listen( PORT || 5000 ,(req, res)=>(
-        console.log(`server is running at port :${PORT}`)
-    ))
-    
-))
-.catch( (error)=>(
-    console.log('App failed to listen due to DB connection failure:',error)
-))
+
+dbconnection()
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((error) => {
+    console.log('App failed to start due to DB connection failure:', error);
+  });
+
+// Export the app for Vercel serverless deployment
+module.exports = app;
