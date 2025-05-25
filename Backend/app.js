1f45a67e794bf61e4435223753efa4dc19745716
manoj2990 <manojmeena2990@gmail.com>
2025-05-25 17:08:18 +0530

    correct url

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: [
         "http://localhost:3000",
         "https://study-notion-manoj2990.vercel.app",
-        "https://shivam-study-notion.netlify.app/"
+        "https://shivam-study-notion.netlify.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
